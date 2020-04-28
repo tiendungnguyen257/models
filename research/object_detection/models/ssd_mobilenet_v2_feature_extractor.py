7ff1f9c86b01d9b8f94fde6f948d489214078903
tiendungnguyen257 <30386144+tiendungnguyen257@users.noreply.github.com>
2020-04-28 17:48:23 +0700

    Update ssd_mobilenet_v2_feature_extractor.py

--- research/object_detection/models/ssd_mobilenet_v2_feature_extractor.py
+++ research/object_detection/models/ssd_mobilenet_v2_feature_extractor.py
@@ -116,7 +116,7 @@ class SSDMobileNetV2FeatureExtractor(ssd_meta_arch.SSDFeatureExtractor):
         'use_explicit_padding': self._use_explicit_padding,
     }
 
-    with tf.variable_scope('MobilenetV2', reuse=self._reuse_weights) as scope:
+    with tf.compat.v1.variable_scope('MobilenetV2', reuse=self._reuse_weights) as scope:
       with slim.arg_scope(
           mobilenet_v2.training_scope(is_training=None, bn_decay=0.9997)), \
           slim.arg_scope(
